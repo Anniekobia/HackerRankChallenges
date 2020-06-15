d707bb038cd6a18bd9126d532e6b1d057f00538e
Anniekobia <kobiaannunziata94@gmail.com>
2020-06-15 18:14:36 +0300

    Solution to check if a string is a palindrome

---
+++ src/StringPalindrome.java
@@ -0,0 +1,32 @@
+import com.sun.org.apache.xerces.internal.util.SynchronizedSymbolTable;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class StringPalindrome {
+    public static boolean isPalindrome(String string){
+        boolean ispalindrome = false;
+        String reversedString = null;
+        char [] myarr = string.toCharArray();
+        ArrayList <Character> mylist = new ArrayList<>();
+        for(int i = myarr.length-1;i>=0;i--){
+            mylist.add(myarr[i]);
+        }
+        reversedString= mylist.stream().map(Object::toString).collect(Collectors.joining());
+        if (string.equals(reversedString)){
+            ispalindrome = true;
+        }
+        System.out.println(ispalindrome);
+        return ispalindrome;
+    }
+    public  static void main(String[] args){
+        System.out.println("Enter String to check if it is a palindrome:");
+
+        Scanner scanner = new Scanner(System.in);
+        String myString = scanner.next();
+        isPalindrome(myString);
+    }
+}
